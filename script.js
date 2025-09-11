42cb71c230ff1e0aec27caa50077008a8684fd58
Your Name <rmarulbabu@gmail.com>
2025-09-11 21:41:26 +0530

    codebyarulcs

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ function scrollToTop() {
 // Typing Effect
 function initializeTypingEffect() {
     const typingElement = document.getElementById('typing-text');
-    const text = 'Frontend Developer & Engineer';
+    const text = ' Full stack Developer';
     let index = 0;
 
     function typeText() {
